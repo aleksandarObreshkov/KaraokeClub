105f61aeed80df9328a0b12b6be51ef672828609
Aleksandar Obreshkov <aleksandar.obreshkov1@gmail.com>
2022-04-23 23:07:33 +0300

    Apply logic onto skeleton

--- main.cpp
+++ main.cpp
@@ -2,32 +2,24 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <pthread.h>
-#include <mutex>
-
 #include <stdio.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <cstdio>
 
-#define SONGS_FIFO "songs_fifo"
+#define SONG_NAME_LENGTH 100
 
 using namespace std;
 
-mutex mtx;
-
-
 list<string> songsToSave;
 list<string> all_songs;
-list<pthread_t*> singers;
 
 typedef struct sing_arguments{
     string song_name;
 }sing_arguments;
 
-void* sing(void* args);
-void* logic(void* args);
+void sing(string song);
 void saveMissingSong(string song);
-void handleThreadCreation(string song);
 
 int main()
 {
@@ -36,42 +28,65 @@ int main()
     cout << "Welcome to the Karaoke Club" << endl;
 
     int numberOfPeopleToSing = rand() % 4 + 1;
-    mkfifo(SONGS_FIFO, 0666);
+    int p[2];
+    if (pipe(p)<0)
+    {
+        cout << "Pipe unsuccessful"<<endl;
+        exit(1);
+    }
 
-    int processId = fork();
+    pid_t processId = fork();
     if (processId == 0)
     {
-        //read a song name from the fifo file
-        //sleep the thread for a certain amount of time
-        int songsPipe = open(SONGS_FIFO, O_RDWR);
-        for (;;)
+        string a;
+        for(;;)
         {
-            string a;
-            read(songsPipe, &a, sizeof(a));
-            if (a == "kill")
+            read(p[0], &a, SONG_NAME_LENGTH);
+            if (a != "killProcess")
             {
-                break;
+                sing(a);
             }
-            if (a!="")
-            {
-                cout << a << endl;
-            }
-            
+            else break;
         }
-        
+        //close(*p[0]);
+        cout << "Closing the stage" << endl;
     }
 
     else 
     {
-        //ask for a song name
-        //if its there, write it to the FIFO file
-        int songsPipe = open(SONGS_FIFO, O_RDWR);
-        string a;
-        cin>>a;
-        write(songsPipe, &a, sizeof(a));
-        
+        for (int i = 0; i < numberOfPeopleToSing; i++)
+        {
+            cout << "Which song would you like to sing?" << endl;
+            string temp;
+            getline(cin, temp);
+            string choice = trimAndLower(temp);
+            if (listContains(all_songs, choice))
+            {
+                write(p[1], &choice, SONG_NAME_LENGTH);
+            }
+            else
+            {
+                saveMissingSong(choice);
+                cout << "We don't have this song right now, would you like to try with another? [y,n]" << endl;
+                char anotherSongFlag;
+                cin >> anotherSongFlag;
+                cin.ignore();
+                if (anotherSongFlag == 'y')
+                {
+                    i--;
+                    continue;
+                }
+                cout << "Hope to see you soon, bye!" << endl;
+            }
         }
-        return 0;
+        write(p[1], "killProcess", SONG_NAME_LENGTH);
+        saveSongs(songsToSave);
+        //close(p[1]);
+        
+    }
+    int status;
+    while ((wait(&status)) > 0);
+    return 0;
 }
 
 void saveMissingSong(string song){
@@ -83,46 +98,9 @@ void saveMissingSong(string song){
     
 }
 
-void* sing(void* params){
-    mtx.lock();
-    sing_arguments* args = (sing_arguments*)params;
-    cout << "Now singing: " << args->song_name << endl;
-    sleep(10);
+void sing(string song){
+    cout << "Now singing: " << song << endl;
+    sleep(3);
     cout << "Singer is done!" << endl;
-    mtx.unlock();
 }
 
-
-// for (int i = 0; i < numberOfPeopleToSing; i++)
-        // {
-        //     cout << "Which song would you like to sing?" << endl;
-        //     string temp;
-        //     getline(cin, temp);
-        //     string choice = trimAndLower(temp);
-        //     if (listContains(all_songs, choice))
-        //     {
-        //         pthread_t singer_thread;
-        //         //singers.push_back(&singer_thread);
-        //         sing_arguments args;
-        //         args.song_name = choice;
-        //         // the thread being created is different every time, but its saved in the same address, same for the struct
-        //         pthread_create(&singer_thread, NULL, sing, &args);
-        //         pthread_join(singer_thread, NULL);
-        //     }
-        //     else
-        //     {
-        //         saveMissingSong(choice);
-        //         cout << "We don't have this song right now, would you like to try with another? [y,n]" << endl;
-        //         char anotherSongFlag;
-        //         cin >> anotherSongFlag;
-        //         cin.ignore();
-        //         if (anotherSongFlag == 'y')
-        //         {
-        //             i--;
-        //             continue;
-        //         }
-        //         cout << "Hope to see you soon, bye!" << endl;
-        //     }
-        // }
-
-        // saveSongs(songsToSave);
